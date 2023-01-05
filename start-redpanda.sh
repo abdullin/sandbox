f7146aa16de3c40cf95f46178f49779e559a34e3
Rinat Abdullin <rinat@abdullin.com>
2023-01-06 00:49:47 +0100

    Created a script to start red panda

---
+++ start-redpanda.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+REMOTE=$(terraform output --raw instance_ip)
+echo $REMOTE
+
+# drop this IP from hosts
+ssh-keygen -R $REMOTE || True
+
+TMP_FILE=docker-compose.yml.tmp
+
+cp docker-compose.yml $TMP_FILE
+sed -i "s/PUBLIC_IP/$REMOTE/" $TMP_FILE
+
+scp $TMP_FILE root@$REMOTE:docker-compose.yml
+rm -rf $TMP_FILE
+
+ssh root@$REMOTE 'docker compose up'
